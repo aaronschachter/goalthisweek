b46de87eaa35f79b42ba43fc1e0534eaf0bdeeb4
aaron schachter <aaronschachter@gmail.com>
2012-09-27 22:09:33 -0400

    remove clearfix from the goal

--- sites/all/modules/goalthisweek/templates/goal.tpl.php
+++ sites/all/modules/goalthisweek/templates/goal.tpl.php
@@ -7,20 +7,22 @@ else {
   $ismine = '';
 }
 ?>
-<div id="the-goal" class="clearfix goal-<?php print $node->nid; ?>">
+<div id="the-goal" class="goal-<?php print $node->nid; ?>">
 
-    <div class="goal-dates left-col">
-      <span class="goal-date-range"><?php print format_date($node->date_start, 'short'); ?> - <?php print format_date($node->date_due, 'short'); ?> </span>
-      <span class="goal-status-icon"><?php print goalthisweek_get_status_icon($node->goal_status) ?></span>
+    <div class="goal-dates">
+      <?php print format_date($node->date_start, 'short'); ?> - <?php print format_date($node->date_due, 'short'); ?>
+   
       
       <?php if (isset($node->title) && !$node->is_due): ?>
       <div><?php print $node->content_goal_status; ?></div>
       <?php elseif (isset($node->title) && $node->is_due): ?>
-      <div><span class="label label-important">Due</span></div>
+       <span class="label label-important">Due</span>
       <?php endif; ?>
       
     </div>
-    <div class="goal-title well right-col">
+    <div class="goal-title well">
+    
+      <span class="pull-right"><?php print goalthisweek_get_status_icon($node->goal_status) ?></span>
       
         <?php if (isset($node->title)): ?>
         <h4><?php print $node->title; ?></h4>  
