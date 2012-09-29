1e03d1e2283ad52296fa3b3937e5c7979764aa02
aaron schachter <aaronschachter@gmail.com>
2012-09-29 09:59:20 -0400

    add social icons to user bar

--- sites/all/modules/goalthisweek/templates/user-bar.tpl.php
+++ sites/all/modules/goalthisweek/templates/user-bar.tpl.php
@@ -1,6 +1,6 @@
 <div class="user-bar">
   <div class="profile-picture">
-  <?php print theme('user_picture', array('account' => $goal_user)); ?>
+    <?php print theme('user_picture', array('account' => $goal_user)); ?>
   </div>
   <h4>
   <?php if (arg(0) == 'user'): ?>
@@ -11,4 +11,5 @@
   </h4>
   <div class="username"><?php print $goal_user->name; ?></div>
   <div class="goal-dates"><?php print $privacy; ?></div>
+  <?php print theme('goalthisweek_user_profile_links', array('goal_user' => $goal_user)); ?>
 </div>
---
+++ sites/all/modules/goalthisweek/templates/user-profile-links.tpl.php
@@ -0,0 +1,33 @@
+<?php 
+if ($goal_user->field_privacy_profile[LANGUAGE_NONE][0]['value'] == 1) {
+  $public = TRUE;
+  $web_url = $goal_user->field_website_url[LANGUAGE_NONE][0]['safe_value'];
+  $fb_url = $goal_user->field_facebook_url[LANGUAGE_NONE][0]['safe_value'];
+  $twitter_url = $goal_user->field_twitter_url[LANGUAGE_NONE][0]['safe_value'];  
+  $linkedin_url = $goal_user->field_linkedin_url[LANGUAGE_NONE][0]['safe_value'];
+}
+?>
+
+<?php if ($public): ?>
+
+<?php if ($web_url): ?>
+<a href="http://<?php print $web_url; ?>" class="user-website-url" target="_blank"><?php print $web_url; ?></a>
+<?php endif; ?>
+
+<div class="user-profile-social-icons">
+
+  <?php if ($fb_url): ?>
+  <a href="http://facebook.com/<?php print $fb_url; ?>" target="_blank"><img src="<?php print GTW_FB_ICON; ?>"></a>
+  <?php endif; ?>
+
+  <?php if ($twitter_url): ?>
+  <a href="http://twitter.com/<?php print $twitter_url; ?>" target="_blank"><img src="<?php print GTW_TWITTER_ICON; ?>"></a>
+  <?php endif; ?>  
+
+  <?php if ($linkedin_url): ?>
+  <a href="http://linkedin.com/in/<?php print $linkedin_url; ?>" target="_blank"><img src="<?php print GTW_LINKEDIN_ICON; ?>"></a>
+  <?php endif; ?>  
+  
+</div>
+
+<?php endif; ?>
