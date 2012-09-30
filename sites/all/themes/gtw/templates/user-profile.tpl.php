08cfe8f4870899a43a8573a42db6f98a9b4b6041
aaron schachter <aaronschachter@gmail.com>
2012-09-30 10:41:03 -0400

    dont allow user/passord or user/register paths

--- sites/all/themes/gtw/templates/user-profile.tpl.php
+++ sites/all/themes/gtw/templates/user-profile.tpl.php
@@ -3,7 +3,7 @@ global $user;
 $user = user_load($user->uid);
 $account_id = arg(1);
 // If this is my profile and i haven't confirmed my account yet, direct to home path
-if ($user->uid == $account_id && $goal_user->field_account_confirmed[LANGUAGE_NONE][0]['value'] != 1) {
+if ($user->uid == $account_id && $user->field_account_confirmed[LANGUAGE_NONE][0]['value'] != 1) {
   drupal_goto('home');
 }
 print goalthisweek_get_profile($account_id);
